cc075e7f975894274601076a6689f9e440f40dfe
Hiromu OCHIAI <otiai10@gmail.com>
2022-12-08 23:16:57 +0900

    Add more detailed error message

--- commands/openaicompletion.go
+++ commands/openaicompletion.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"context"
+	"fmt"
 	"math/rand"
 
 	"github.com/otiai10/amesh-bot/service"
@@ -25,8 +26,13 @@ func (cmd AICompletion) Execute(ctx context.Context, client service.ISlackClient
 	tokens := largo.Tokenize(event.Text)[1:]
 	ai := openai.Client{APIKey: cmd.APIKey, BaseURL: cmd.BaseURL}
 	res, err := ai.Ask(openai.Davinci, tokens)
-	if err != nil || len(res.Choices) == 0 {
-		return NotFound{}.Execute(ctx, client, event)
+	if err != nil {
+		nferr := NotFound{}.Execute(ctx, client, event)
+		return fmt.Errorf("openai.Ask failed with: %v (and NotFound Cmd error: %v)", err, nferr)
+	}
+	if len(res.Choices) == 0 {
+		nferr := NotFound{}.Execute(ctx, client, event)
+		return fmt.Errorf("openai.Ask returns zero choice (and NotFound Cmd error: %v)", nferr)
 	}
 	msg.Text = res.Choices[rand.Intn(len(res.Choices))].Text
 	_, err = client.PostMessage(ctx, msg)
